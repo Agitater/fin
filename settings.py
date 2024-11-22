3a9c112c9debbc628ea769da285a8d0d1386b9e2
Agitater <67369248+Agitater@users.noreply.github.com>
2024-11-22 08:18:44 +0000

    22

---
+++ settings.py
@@ -0,0 +1,23 @@
+# defining colours
+white = (255, 255, 255)
+black = (0, 0, 0)
+grey = (40, 40, 40)
+lgrey = (100, 100, 100)
+green = (0, 255, 0)
+red = (255, 0, 0)
+yellow = (255, 255, 0)
+
+# game settings
+width = 1024
+height = 768
+fps = 60
+title = "belly buddies"
+backgroundcolour = grey
+
+tilesize = 32
+gridwidth = width / tilesize
+gridheight = height / tilesize
+
+# player settings
+player_speed = 100
+# this 8am
